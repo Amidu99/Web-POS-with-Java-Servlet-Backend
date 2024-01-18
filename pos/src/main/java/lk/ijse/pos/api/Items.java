b66f323d2394424f5f7529db9f6f1b9bea4aaa7d
Amidu99 <amidushamika@gmail.com>
2024-01-18 22:25:10 +0530

    complete doPut method

--- pos/src/main/java/lk/ijse/pos/api/Items.java
+++ pos/src/main/java/lk/ijse/pos/api/Items.java
@@ -46,4 +46,18 @@ public class Items extends HttpServlet {
             dbProcess.saveNewItem(itemDTO,connection);
         }
     }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if(req.getContentType() == null ||
+            !req.getContentType().toLowerCase().startsWith("application/json")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }else{
+            logger.info("Start Item Servlet doPut method.");
+            Jsonb jsonb = JsonbBuilder.create();
+            var itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+            var dbProcess = new DBProcess();
+            dbProcess.updateItem(itemDTO, connection);
+        }
+    }
 }
