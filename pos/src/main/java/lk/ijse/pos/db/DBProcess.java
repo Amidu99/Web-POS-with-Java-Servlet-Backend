04a3ced73fc047c8606a014d33df99e13689860b
Amidu99 <amidushamika@gmail.com>
2024-01-18 21:30:52 +0530

    complete update customer method

--- pos/src/main/java/lk/ijse/pos/db/DBProcess.java
+++ pos/src/main/java/lk/ijse/pos/db/DBProcess.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 public class DBProcess {
     final static Logger logger = LoggerFactory.getLogger(DBProcess.class);
     private static final String SAVE_CUSTOMER_DATA = "INSERT INTO customer (customer_id, name, address, salary) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_CUSTOMER_DATA = "UPDATE customer SET name=?, address=?, salary=? WHERE customer_id=?";
 
     public void saveNewCustomer(CustomerDTO customer, Connection connection) {
         logger.info("Start saveNewCustomer method.");
@@ -23,6 +24,25 @@ public class DBProcess {
                 logger.info("Customer saved.");
             } else {
                 logger.error("Failed to save the Customer.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateCustomer(CustomerDTO customer, Connection connection) {
+        logger.info("Start updateCustomer method.");
+        try {
+            var ps = connection.prepareStatement(UPDATE_CUSTOMER_DATA);
+            ps.setString(1, customer.getName());
+            ps.setString(2, customer.getAddress());
+            ps.setDouble(3, customer.getSalary());
+            ps.setString(4, customer.getCustomer_id());
+
+            if (ps.executeUpdate() != 0) {
+                logger.info("Customer updated.");
+            } else {
+                logger.error("Failed to update the Customer.");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
