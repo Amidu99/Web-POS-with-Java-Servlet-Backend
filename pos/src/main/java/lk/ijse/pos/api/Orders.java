57629548de9502beb9f73e4d056a773f7f9dd6bc
Amidu99 <amidushamika@gmail.com>
2024-01-18 22:55:03 +0530

    add doPost method

--- pos/src/main/java/lk/ijse/pos/api/Orders.java
+++ pos/src/main/java/lk/ijse/pos/api/Orders.java
@@ -1,12 +1,19 @@
 package lk.ijse.pos.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.pos.db.DBProcess;
+import lk.ijse.pos.dto.OrderinfoDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -23,6 +30,20 @@ public class Orders extends HttpServlet {
             this.connection = pool.getConnection();
         } catch (NamingException | SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if(req.getContentType() == null ||
+            !req.getContentType().toLowerCase().startsWith("application/json")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }else{
+            logger.info("Start Order Servlet doPost method.");
+            Jsonb jsonb = JsonbBuilder.create();
+            var orderinfoDTO = jsonb.fromJson(req.getReader(), OrderinfoDTO.class);
+            var dbProcess = new DBProcess();
+            dbProcess.saveNewOrder(orderinfoDTO,connection);
         }
     }
 }
