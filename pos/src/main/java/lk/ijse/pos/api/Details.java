167d7b9a933dc2d52d4fd707e0353c8da4d5318f
Amidu99 <amidushamika@gmail.com>
2024-01-18 23:32:35 +0530

    complete doPost method

--- pos/src/main/java/lk/ijse/pos/api/Details.java
+++ pos/src/main/java/lk/ijse/pos/api/Details.java
@@ -1,14 +1,23 @@
 package lk.ijse.pos.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.pos.db.DBProcess;
+import lk.ijse.pos.dto.OrderdetailsDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "details",urlPatterns = "/details")
 public class Details extends HttpServlet {
@@ -23,6 +32,24 @@ public class Details extends HttpServlet {
             this.connection = pool.getConnection();
         } catch (NamingException | SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if(req.getContentType() == null ||
+            !req.getContentType().toLowerCase().startsWith("application/json")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }else{
+            logger.info("Start Details Servlet doPost method.");
+            Jsonb jsonb = JsonbBuilder.create();
+            var dbProcess = new DBProcess();
+
+            // Handle JSON array
+            List<OrderdetailsDTO> dtoList= jsonb.fromJson(req.getReader(),new ArrayList<OrderdetailsDTO>(){
+            }.getClass().getGenericSuperclass());
+            dbProcess.saveOrderDetails(dtoList,connection);
+            jsonb.toJson(dtoList,resp.getWriter());
         }
     }
 }
